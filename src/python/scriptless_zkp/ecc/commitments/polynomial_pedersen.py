9a4642d657101f17745b9c0035da3a8e413da82d
Will D. Spann <qubits4all@users.noreply.github.com>
2025-09-07 23:43:59 -0600

    [zkp] Refactor NIZK proof generation & verification
    - Refactored Polynomial Pedersen evaluation proof generation, to be a
    method of the `RevealedPolynomialPedersenCommitment` class, instead of a
    method of `PolynomialPedersenCommitmentContexst`.
    
    - Created a new `PolynomialPedersenEvaluationProof` dataclass, and
    refactored proof evaluation to be a method of this class, instead of a
    method of `SealedPolynomialPedersenCommitment`.
      - Retained a proof verification method on
      `SealedPolynomialPedersenCommitment`, which now simply calls the new
      verification method on the proof's object instead, passing the sealed
      commitment as an argument.
    
    GI-37

--- src/python/scriptless_zkp/ecc/commitments/polynomial_pedersen.py
+++ src/python/scriptless_zkp/ecc/commitments/polynomial_pedersen.py
@@ -46,7 +46,7 @@ from scriptless_zkp.ecc.commitments.pedersen import (
 from scriptless_zkp.ecc.weierstrass_curves import WeierstrassEllipticCurveConfig
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True)
 class SealedPolynomialPedersenCommitment:
     coefficient_commitments: list[SealedPedersenCommitment]
     field: type[FieldArray]
@@ -76,53 +76,11 @@ class SealedPolynomialPedersenCommitment:
             for i in range(len(self.coefficient_commitments))
         )
 
-    def verify_evaluation_proof(self, x: int, y: int, proof: int) -> bool:
-        """
-        Verifies a non-interactive zero-knowledge (NIZK) proof that the committed polynomial evaluates to `y` at the
-        given `x` value, without revealing any information about the polynomial itself.
-
-        This verification checks that the following equation holds (i.e., the elliptic curve points produced by the
-        left & right sides are equal), using the coefficient commitments `[C_i]_d` from this
-        `SealedPolynomialPedersenCommitment`:
-
-            `y*G + 𝜋*B == C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d`,
-
-        where `C_i` are the coefficient commitments, `G` is the Pedersen commitment base point (common generator),
-        `B` is the Pedersen commitment NUMS generator, `p` is the field characteristic, and `d` is the degree of the
-        polynomial.
-
-        :param x: the `x` value at which the committed polynomial was evaluated by the prover, which must be in the
-                  range `[0, p)` where `p` is the field characteristic, as provided to the prover by the verifier.
-        :param y: the result of evaluating the committed polynomial at the given `x` value, as provided by the prover.
-        :param proof: the NIZK proof that the committed polynomial evaluates to `y` at `x`, as provided by the prover.
-        :return: True if the proof is valid (i.e., the proof verification equation holds); False otherwise.
-        :raises ValueError: if the provided `x` or `y` values are out of range for the field `F_p` (i.e., not in the
-                            range `[0, p)` where `p` is the field characteristic).
-        """
-        if not (0 <= x < self.field.characteristic):
-            raise ValueError(f"Evaluation point x is out of range for the field F_{self.field.characteristic}.")
-        if not (0 <= y < self.field.characteristic):
-            raise ValueError(f"Evaluation result y is out of range for the field F_{self.field.characteristic}.")
-
-        G: ECC.EccPoint = self.curve_config.base_point
-        B: ECC.EccPoint = self.nums_generator
-
-        # Left side: y*G + 𝜋*B
-        left_side: ECC.EccPoint = G * y + B * proof
-
-        # Right side: C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d
-        right_side: ECC.EccPoint = reduce(
-            lambda accum, pt:
-                accum + pt,  # elliptic curve point addition
-            [self.coefficient_commitments[i].commitment_point * pow(x, i, self.field.characteristic)
-             for i in range(len(self.coefficient_commitments))],
-            self.curve_config.identity  # start point additions w/ curve's identity element: O (i.e., point-at-infinity)
-        )
-
-        return left_side == right_side
+    def verify_evaluation_proof(self, proof: PolynomialPedersenEvaluationProof) -> bool:
+        return proof.verify(self)
 
 
-@dataclass(frozen=True, slots=True)
+@dataclass(frozen=True)
 class RevealedPolynomialPedersenCommitment:
     coefficient_commitments: list[RevealedPedersenCommitment]
     blinding_factors: list[int]
@@ -148,11 +106,131 @@ class RevealedPolynomialPedersenCommitment:
     def nums_generator(self) -> ECC.EccPoint:
         return self.coefficient_commitments[0].nums_generator
 
+    def generate_evaluation_proof(self, x: int) -> PolynomialPedersenEvaluationProof:
+        """
+        Generates a non-interactive zero-knowledge (NIZK) proof that the committed polynomial evaluates to `y` at the
+        given `x` value, without revealing any information about the polynomial itself.
+
+        0. Precondition: The prover has already committed to a secret polynomial using a Polynomial Pedersen commitment,
+        resulting in a `SealedPolynomialPedersenCommitment` and a corresponding `RevealedPolynomialPedersenCommitment`
+        (which includes the secret blinding factors used to produce the coefficient commitments).
+            a. The prover wants to prove to a verifier that the committed polynomial evaluates to `y` at a specific `x`
+               value, without revealing the polynomial itself.
+            b. The verifier knows the `SealedPolynomialPedersenCommitment` and the `x` value, but does not know the
+               polynomial or its coefficients.
+        1. The prover and verifier agree on the `x` value at which the polynomial will be evaluated, which must be in
+        the range `[0, p)` where `p` is the field characteristic, with the verifier providing the `x` value to the
+        prover.
+        2. The prover evaluates the committed polynomial at the given `x` value to obtain `y`.
+        3. The prover computes a non-interactive zero-knowledge proof (NIZK) that the committed polynomial evaluates to
+        `y` at the given `x` value, using the secret blinding factors from its (revealed) coefficient commitments.
+            - This proof `𝜋` is computed as:
+                `𝜋 := r_0 + r_1*x + r_2*x^2 + ... + r_d*x^d (mod p)`,
+            where `r_i` are the blinding factors for each coefficient commitment, `p` is the field characteristic,
+            and `d` is the degree of the polynomial.
+        4. The prover sends the pair `(y, 𝜋)` of integers in `Z_p` to the verifier.
+        5. The verifier checks that the proof is valid by verifying that the following equation holds
+        (i.e., the elliptic curve points produced by the left & right sides are equal), using the coefficient
+        commitments from the `SealedPolynomialPedersenCommitment`, provided to the verifier by the prover earlier:
+            `y*G + 𝜋*B == C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d`,
+        where `C_i` are the coefficient commitments, `G` is the Pedersen commitment base point (common generator), and
+        `B` is the Pedersen commitment NUMS generator, `p` is the field characteristic, and `d` is the degree of the
+        polynomial.
+
+        :param x: the `x` value at which to evaluate the committed polynomial, which must be in the range `[0, p)` where
+                  `p` is the field characteristic.
+        :return: a `PolynomialPedersenEvaluationProof` proving in zero-knowledge the evaluation of the committed
+                 polynomial at the provided `x` value equals `y`, encapsulated as the tuple `(x, y, proof)` of integers
+                 in `Z_p`, where `p` is the field characteristic.
+        :raises ValueError: if the provided `x` value is out of range for the field (i.e., not in the range `[0, p)`
+                where `p` is the field characteristic).
+        """
+        if not (0 <= x < self.field.characteristic):
+            raise ValueError(f"Evaluation point x is out of range for the field F_{self.field.characteristic}.")
+
+        # Evaluate the committed polynomial at the given x value.
+        y: int = int(self.committed_polynomial(x))
+
+        # Compute a zero-knowledge proof that the committed polynomial evaluates to y at x:
+        #     `𝜋 := r_0 + r_1*x + r_2*x^2 + ... + r_d*x^d (mod p)`,
+        # where `r_i` are the blinding factors for each coefficient commitment, `p` is the field characteristic,
+        # and `d` is the degree of the polynomial.
+        proof: int = sum(
+            blinding_factor * pow(x, i, self.field.characteristic)
+            for i, blinding_factor in enumerate(self.blinding_factors)
+        ) % self.field.characteristic
+
+        # Return the NIZK polynomial evaluation proof, which encapsulates the tuple `(x, y, 𝜋)` of integers modulo `p`.
+        return PolynomialPedersenEvaluationProof(x, y, proof)
+
+
+@dataclass(frozen=True)
+class PolynomialPedersenEvaluationProof:
+    x: int
+    y: int
+    proof: int
+
+    @property
+    def evaluation(self) -> int:
+        return self.y
+
+    def verify(
+            self,
+            sealed_commitment: SealedPolynomialPedersenCommitment
+    ) -> bool:
+        """
+        Verifies a non-interactive zero-knowledge (NIZK) proof that the committed polynomial evaluates to `y` at the
+        given `x` value, without revealing any information about the polynomial itself.
+
+        This verification checks that the following equation holds (i.e., the elliptic curve points produced by the
+        left & right sides are equal), using the coefficient commitments `[C_i]_d` from this
+        `SealedPolynomialPedersenCommitment`:
+
+            `y*G + 𝜋*B == C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d`,
+
+        where `C_i` are the coefficient commitments, `G` is the Pedersen commitment base point (common generator),
+        `B` is the Pedersen commitment NUMS generator, `p` is the field characteristic, and `d` is the degree of the
+        polynomial.
+
+        :param sealed_commitment: the `SealedPolynomialPedersenCommitment` containing the coefficient commitments
+                                  corresponding to the committed polynomial, for which this zero-knowledge evaluation
+                                  proof is being verified.
+        :return: True if the proof is valid (i.e., the proof verification equation holds); False otherwise.
+        :raises ValueError: if the provided `x` or `y` values are out of range for the field `F_p` (i.e., not in the
+                            range `[0, p)` where `p` is the field characteristic).
+        """
+        if not (0 <= self.x < sealed_commitment.field.characteristic):
+            raise ValueError(
+                f"Evaluation point 'x' is out of range for the field 'F_{sealed_commitment.field.characteristic}'."
+            )
+        if not (0 <= self.y < sealed_commitment.field.characteristic):
+            raise ValueError(
+                f"Evaluation result 'y' is out of range for the field 'F_{sealed_commitment.field.characteristic}'."
+            )
+
+        G: ECC.EccPoint = sealed_commitment.curve_config.base_point
+        B: ECC.EccPoint = sealed_commitment.nums_generator
+
+        # Left side: y*G + 𝜋*B
+        left_side: ECC.EccPoint = G * self.y + B * self.proof
+
+        # Right side: C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d
+        right_side: ECC.EccPoint = reduce(
+            lambda accum, pt:
+            accum + pt,  # elliptic curve point addition
+            [sealed_commitment.coefficient_commitments[i].commitment_point
+                * pow(self.x, i, sealed_commitment.field.characteristic)
+             for i in range(len(sealed_commitment.coefficient_commitments))],
+            sealed_commitment.curve_config.identity  # start point additions w/ curve's identity element: O (i.e., point-at-infinity)
+        )
+
+        return left_side == right_side
+
 
 class PolynomialPedersenContext:
     def __init__(self, field: type[FieldArray], pedersen_context: PedersenCommitmentContext):
         if not galois.is_prime(field.characteristic):
-            raise ValueError("Field must be a field with prime characteristic (e.g., F_p where p is prime, or F_p^m).")
+            raise ValueError("Field must be a field with prime characteristic (e.g., F_p or F_p^m, where p is prime).")
 
         self.field = field
         self.pedersen_context = pedersen_context
@@ -195,65 +273,3 @@ class PolynomialPedersenContext:
             blinding_factors=[rc.blinding_factor for rc in revealed_coefficient_commitments],
             committed_polynomial=polynomial
         )
-
-    def generate_evaluation_proof(
-            self,
-            revealed_commitment: RevealedPolynomialPedersenCommitment,
-            x: int
-    ) -> tuple[int, int]:
-        """
-        Generates a non-interactive zero-knowledge (NIZK) proof that the committed polynomial evaluates to `y` at the
-        given `x` value, without revealing any information about the polynomial itself.
-
-        0. Precondition: The prover has already committed to a secret polynomial using a Polynomial Pedersen commitment,
-        resulting in a `SealedPolynomialPedersenCommitment` and a corresponding `RevealedPolynomialPedersenCommitment`
-        (which includes the secret blinding factors used to produce the coefficient commitments).
-            a. The prover wants to prove to a verifier that the committed polynomial evaluates to `y` at a specific `x`
-               value, without revealing the polynomial itself.
-            b. The verifier knows the `SealedPolynomialPedersenCommitment` and the `x` value, but does not know the
-               polynomial or its coefficients.
-        1. The prover and verifier agree on the `x` value at which the polynomial will be evaluated, which must be in
-        the range `[0, p)` where `p` is the field characteristic, with the verifier providing the `x` value to the
-        prover.
-        2. The prover evaluates the committed polynomial at the given `x` value to obtain `y`.
-        3. The prover computes a non-interactive zero-knowledge proof (NIZK) that the committed polynomial evaluates to
-        `y` at the given `x` value, using the secret blinding factors from its (revealed) coefficient commitments.
-            - This proof `𝜋` is computed as:
-                `𝜋 := r_0 + r_1*x + r_2*x^2 + ... + r_d*x^d (mod p)`,
-            where `r_i` are the blinding factors for each coefficient commitment, `p` is the field characteristic,
-            and `d` is the degree of the polynomial.
-        4. The prover sends the pair `(y, 𝜋)` of integers in `Z_p` to the verifier.
-        5. The verifier checks that the proof is valid by verifying that the following equation holds
-        (i.e., the elliptic curve points produced by the left & right sides are equal), using the coefficient
-        commitments from the `SealedPolynomialPedersenCommitment`, provided to the verifier by the prover earlier:
-            `y*G + 𝜋*B == C_0 + C_1*x + C_2*x^2 + ... + C_d*x^d`,
-        where `C_i` are the coefficient commitments, `G` is the Pedersen commitment base point (common generator), and
-        `B` is the Pedersen commitment NUMS generator, `p` is the field characteristic, and `d` is the degree of the
-        polynomial.
-
-        :param revealed_commitment: the revealed polynomial commitment to the secret polynomial's coefficients, which
-               includes the blinding factors used in the coefficient commitments and the committed polynomial itself
-               (provided by the prover).
-        :param x: the `x` value at which to evaluate the committed polynomial, which must be in the range `[0, p)` where
-                  `p` is the field characteristic.
-        :return: the pair (y, proof) where `y` is the result of evaluating the committed polynomial at the given `x`
-                 value, and `proof` is the NIZK proof that the committed polynomial evaluates to `y` at `x`.
-        :raises ValueError: if the provided `x` value is out of range for the field.
-        """
-        if not (0 <= x < self.field.characteristic):
-            raise ValueError(f"Evaluation point x is out of range for the field F_{self.field.characteristic}.")
-
-        # Evaluate the committed polynomial at the given x value.
-        y: int = int(revealed_commitment.committed_polynomial(x))
-
-        # Compute a zero-knowledge proof that the committed polynomial evaluates to y at x:
-        #     `𝜋 := r_0 + r_1*x + r_2*x^2 + ... + r_d*x^d (mod p)`,
-        # where `r_i` are the blinding factors for each coefficient commitment, `p` is the field characteristic,
-        # and `d` is the degree of the polynomial.
-        proof: int = sum(
-            blinding_factor * pow(x, i, self.field.characteristic)
-            for i, blinding_factor in enumerate(revealed_commitment.blinding_factors)
-        ) % self.field.characteristic
-
-        # Return the evaluation/proof pair (y, 𝜋), of integers modulo `p`.
-        return y, proof
