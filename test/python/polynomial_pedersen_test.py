20f05111541104a757748a4f256c768a911d95aa
Will D. Spann <qubits4all@users.noreply.github.com>
2025-10-04 16:11:24 -0600

    [test] Revise Polynomial Pedersen Eval. Proof test
    - Revised an initial test for producing & verifying a Polynomial Pedersen
    Commitment Evaluation proof, in order to attempt to initialize the
    `galois.GF` Galois field with modulus equal to the elliptic curve sub-
    group's order, using an optimization involving passing the
    pre-constructed irreducible polynomial (a Conway polynomial) via one of
    the `galois.Poly` factory methods. (WIP)
    
    GI-37

--- test/python/polynomial_pedersen_test.py
+++ test/python/polynomial_pedersen_test.py
@@ -1,6 +1,6 @@
 import galois
 
-from galois import GF
+from galois import GF, Poly
 
 from scriptless_zkp.ecc.weierstrass_curves import WeierstrassEllipticCurveConfig
 from scriptless_zkp.ecc.commitments.pedersen import PedersenCommitmentContext
@@ -8,9 +8,23 @@ from scriptless_zkp.ecc.commitments.polynomial_pedersen import PolynomialPederse
 
 
 pedersen_context = PedersenCommitmentContext.for_curve(curve_config=WeierstrassEllipticCurveConfig.secp256r1())
-GF17 = GF(17)
-poly_context = PolynomialPedersenContext(field=GF17, pedersen_context=pedersen_context)
-poly: galois.Poly = galois.Poly([1, 0, 13, 7, 4, 10, 0, 2, 16], field=GF17)
+# GF17 = GF(17)
+
+# Optimization: Provide the (Conway) irreducible polynomial basis for the Galois field & a primitive element explicitly.
+#   <class 'galois.GF(115792089210356248762697446949407573529996955224135760342422259061068512044369, primitive_element='7', irreducible_poly='x + 115792089210356248762697446949407573529996955224135760342422259061068512044362')'>
+irreducible_poly: Poly = galois.Poly([1, 115792089210356248762697446949407573529996955224135760342422259061068512044362])
+GFq = GF(
+    pedersen_context.curve_config.order,
+    irreducible_poly=Poly.Str("x + 115792089210356248762697446949407573529996955224135760342422259061068512044362"),  # x + (q-7)
+    primitive_element=7,
+    verify=False,
+    compile='python-calculate'
+)
+# GFq = GF(int(pedersen_context.curve_config.order))
+
+
+poly_context = PolynomialPedersenContext(field=GFq, pedersen_context=pedersen_context)
+poly: galois.Poly = galois.Poly([1, 0, 13, 7, 4, 10, 0, 2, 16], field=GFq)
 sealed_poly_commitment, revealed_poly_commitment = poly_context.commit_to_polynomial(poly)
 
 # poly(6)
@@ -21,4 +35,7 @@ proof: PolynomialPedersenEvaluationProof = revealed_poly_commitment.generate_eva
 # proof
 # >> PolynomialPedersenEvaluationProof(x=6, y=10, proof=1)
 
-proof.verify(sealed_poly_commitment)
+if proof.verify(sealed_poly_commitment):
+    print("Valid Pedersen Polynomial evaluation proof")
+else:
+    print("Invalid Pedersen Polynomial evaluation proof")
